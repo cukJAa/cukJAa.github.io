18dfeb75435af94caf467f00ebfe7b666805d958
cukJAa <jurgencukaj@gmail.com>
2026-03-16 18:04:01 +0100

    New project added

---
+++ deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+echo "Script started at $(date)"
+
+echo "Current directory: $(pwd)"
+# cat projects.json
+# jq '.[].title' projects.json 
+# projects_titles=($(jq '.[].title' projects.json ))
+
+readarray -t projects_titles < <(jq -r '.[].title' projects.json)
+count=${#projects_titles[@]}
+echo "Projects: ${projects_titles[@]}"
+echo "Number of project: $count"
+
+cd ../projects
+echo "Current directory: $(pwd)"
+find . -maxdepth 1 -type d ! -path .
+readarray -t folders_names < <(find . -maxdepth 1 -type d ! -path .)
+echo "Array of folders names: ${folders_names[@]}"
+count_folders=${#folders_names[@]}
+echo "Number of fodlers: $count_folders"
+
+if [ $count = $count_folders ]; then 
+    echo "The number of Json titles is: $count the same with the number of folders in directory projects which is: $count_folders"
+else 
+    echo "Current directory $(pwd)"
+    cd ../portofolioMain 
+    python3 build_projects.py
+    echo "Current directory $(pwd)"
+    git add .
+    git commit -m "New project added"
+    git push -u origin
+fi
+
+
+
+
+
+
+
+
+# Add the shebang line (#!/bin/bash)
+# Print a message so you know the script started (use echo)
+# Run python3 build_projects.py to regenerate projects.json
+# cd into projects folder and we print if we in the right folder
+# count the number of object in projects.json and maybe you can print out that one 
+
+
